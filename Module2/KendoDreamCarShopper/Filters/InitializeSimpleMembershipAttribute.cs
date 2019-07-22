7cfb4f8155a5d76467d14f4a9cf98afb6ae27b52
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-07-22 22:04:11 +0300

    Fixed connection string in InitializeSimpleMembershipAttribute.

--- Module2/KendoDreamCarShopper/Filters/InitializeSimpleMembershipAttribute.cs
+++ Module2/KendoDreamCarShopper/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace KendoDreamCarShopper.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("EntityStore", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
